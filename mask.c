87cd62063b56aa02a9141ff1809dac06d5f86b69
lamia.dendani <lamia.dendani@ens.uvsq.fr>
2023-02-23 17:11:41 +0100

    ajout de version unrolling

--- mask.c
+++ mask.c
@@ -16,7 +16,7 @@
 #define ERR_CREAT_FILE   7
 
 //
-#define ALIGN64 64
+#define ALIGN 64
 
 typedef double f64;
 
@@ -77,7 +77,7 @@ seq_t *restrict load_seq(const char *fname)
     }
   
   //Allocate sequence 
-  seq_t *restrict s = aligned_alloc(ALIGN64 , sizeof(seq_t));
+  seq_t *restrict s = aligned_alloc(ALIGN , sizeof(seq_t));
   
   if (!s)
     {
@@ -89,7 +89,7 @@ seq_t *restrict load_seq(const char *fname)
   s->len = sb.st_size;
 
   //Allocating memory for sequence bases
-  s->bases = aligned_alloc(ALIGN64 , sizeof(u8) * sb.st_size);
+  s->bases = aligned_alloc(ALIGN , sizeof(u8) * sb.st_size);
   
   if (!s->bases)
     {
@@ -142,7 +142,8 @@ void release_seq(seq_t *s)
     err_id = ERR_NULL_POINTER;
 }
 
-//
+//version de base "Naive"
+
 void mask(const u8 *restrict a, const u8 *restrict b, u8 *restrict c, u64 n)
 {  
   // x = load(a)
@@ -153,12 +154,124 @@ void mask(const u8 *restrict a, const u8 *restrict b, u8 *restrict c, u64 n)
   for (u64 i = 0; i < n; i++)
     c[i] = a[i] ^ b[i];
 }
+
+
+//version Unroll 
+#if __AVX512F__
 void mask_unroll(const u8 *restrict a, const u8 *restrict b, u8 *restrict c, u64 n)
 {  
   //
-  for (u64 i = 0; i < n; i++)  
-    c[i] = a[i] ^ b[i];
+  for (u64 i = 0; i < n; i+=64) 
+  {
+    c[i+0] = a[i+0]  ^ b[i+0] ;
+    c[i+1] = a[i+1]  ^ b[i+1] ;
+    c[i+2] = a[i+2]  ^ b[i+2] ;
+    c[i+3] = a[i+3]  ^ b[i+3] ;
+    c[i+4] = a[i+4]  ^ b[i+4] ;
+    c[i+5] = a[i+5]  ^ b[i+5] ;
+    c[i+6] = a[i+6]  ^ b[i+6] ;
+    c[i+7] = a[i+7]  ^ b[i+7] ;
+    c[i+8] = a[i+8]  ^ b[i+8] ;
+    c[i+9] = a[i+0] ^ b[i+0];
+    c[i+10] = a[i+10] ^ b[i+10];
+    c[i+11] = a[i+11] ^ b[i+11];
+    c[i+12] = a[i+12] ^ b[i+12];
+    c[i+13] = a[i+13] ^ b[i+13];
+    c[i+14] = a[i+14] ^ b[i+14];
+    c[i+15] = a[i+15] ^ b[i+15];
+    c[i+16] = a[i+16] ^ b[i+16];
+    c[i+17] = a[i+17] ^ b[i+17];
+    c[i+18] = a[i+18] ^ b[i+18];
+    c[i+19] = a[i+19] ^ b[i+19];
+    c[i+20] = a[i+20] ^ b[i+20];
+    c[i+21] = a[i+21] ^ b[i+21];
+    c[i+22] = a[i+22] ^ b[i+22];
+    c[i+23] = a[i+23] ^ b[i+23];
+    c[i+24] = a[i+24] ^ b[i+24];
+    c[i+25] = a[i+25] ^ b[i+25];
+    c[i+26] = a[i+26] ^ b[i+26];
+    c[i+27] = a[i+27] ^ b[i+27];
+    c[i+28] = a[i+28] ^ b[i+28];
+    c[i+29] = a[i+29] ^ b[i+29];
+    c[i+30] = a[i+30] ^ b[i+30];
+    c[i+31] = a[i+31] ^ b[i+31];
+    c[i+32] = a[i+32] ^ b[i+32];
+    c[i+33] = a[i+33] ^ b[i+33];
+    c[i+34] = a[i+34] ^ b[i+34];
+    c[i+35] = a[i+35] ^ b[i+35];
+    c[i+36] = a[i+36] ^ b[i+36];
+    c[i+37] = a[i+37] ^ b[i+37];
+    c[i+38] = a[i+38] ^ b[i+38];
+    c[i+39] = a[i+39] ^ b[i+39];
+    c[i+41] = a[i+41] ^ b[i+41];
+    c[i+42] = a[i+42] ^ b[i+42];
+    c[i+43] = a[i+43] ^ b[i+43];
+    c[i+44] = a[i+44] ^ b[i+44];
+    c[i+45] = a[i+45] ^ b[i+45];
+    c[i+46] = a[i+46] ^ b[i+46];
+    c[i+47] = a[i+47] ^ b[i+47];
+    c[i+48] = a[i+48] ^ b[i+48];
+    c[i+49] = a[i+49] ^ b[i+49];
+    c[i+50] = a[i+50] ^ b[i+50];
+    c[i+51] = a[i+51] ^ b[i+51];
+    c[i+52] = a[i+52] ^ b[i+52];
+    c[i+53] = a[i+53] ^ b[i+53];
+    c[i+54] = a[i+54] ^ b[i+54];
+    c[i+55] = a[i+55] ^ b[i+55];
+    c[i+56] = a[i+56] ^ b[i+56];
+    c[i+57] = a[i+57] ^ b[i+57];
+    c[i+58] = a[i+58] ^ b[i+58];
+    c[i+59] = a[i+59] ^ b[i+59];
+    c[i+60] = a[i+60] ^ b[i+60];
+    c[i+61] = a[i+61] ^ b[i+61];
+    c[i+62] = a[i+62] ^ b[i+62];
+    c[i+63] = a[i+63] ^ b[i+63];
+  } 
+    
 }
+
+#else
+void mask_unroll(const u8 *restrict a, const u8 *restrict b, u8 *restrict c, u64 n)
+{  
+  //
+  for (u64 i = 0; i < n; i+=32)  
+    {
+      c[i+0] = a[i+0]  ^ b[i+0] ;
+      c[i+1] = a[i+1]  ^ b[i+1] ;
+      c[i+2] = a[i+2]  ^ b[i+2] ;
+      c[i+3] = a[i+3]  ^ b[i+3] ;
+      c[i+4] = a[i+4]  ^ b[i+4] ;
+      c[i+5] = a[i+5]  ^ b[i+5] ;
+      c[i+6] = a[i+6]  ^ b[i+6] ;
+      c[i+7] = a[i+7]  ^ b[i+7] ;
+      c[i+8] = a[i+8]  ^ b[i+8] ;
+      c[i+9] = a[i+0] ^ b[i+0];
+      c[i+10] = a[i+10] ^ b[i+10];
+      c[i+11] = a[i+11] ^ b[i+11];
+      c[i+12] = a[i+12] ^ b[i+12];
+      c[i+13] = a[i+13] ^ b[i+13];
+      c[i+14] = a[i+14] ^ b[i+14];
+      c[i+15] = a[i+15] ^ b[i+15];
+      c[i+16] = a[i+16] ^ b[i+16];
+      c[i+17] = a[i+17] ^ b[i+17];
+      c[i+18] = a[i+18] ^ b[i+18];
+      c[i+19] = a[i+19] ^ b[i+19];
+      c[i+20] = a[i+20] ^ b[i+20];
+      c[i+21] = a[i+21] ^ b[i+21];
+      c[i+22] = a[i+22] ^ b[i+22];
+      c[i+23] = a[i+23] ^ b[i+23];
+      c[i+24] = a[i+24] ^ b[i+24];
+      c[i+25] = a[i+25] ^ b[i+25];
+      c[i+26] = a[i+26] ^ b[i+26];
+      c[i+27] = a[i+27] ^ b[i+27];
+      c[i+28] = a[i+28] ^ b[i+28];
+      c[i+29] = a[i+29] ^ b[i+29];
+      c[i+30] = a[i+30] ^ b[i+30];
+      c[i+31] = a[i+31] ^ b[i+31];
+    }
+    
+} 
+#endif
 
 //
 void measure_mask(const char *title,
@@ -171,7 +284,7 @@ void measure_mask(const char *title,
   f64 elapsed = 0.0;
   struct timespec t1, t2;
 
-  u8 *restrict cmp_mask = aligned_alloc(ALIGN64 ,sizeof(u8) * n);
+  u8 *restrict cmp_mask = aligned_alloc(ALIGN ,sizeof(u8) * n);
 
   FILE *fp = fopen("mask.dat", "wb");
 
@@ -243,6 +356,7 @@ int main(int argc, char **argv)
   
   //
   measure_mask("Naive", mask, s1->bases, s2->bases, s1->len);
+  measure_mask("Unroll", mask_unroll, s1->bases, s2->bases, s1->len);
   
   //
   release_seq(s1); free(s1);
